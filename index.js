91039cad1f7314e8206285466bfc350bfc877122
kgayathri4 <04gayathri.k@gmail.com>
2025-11-13 16:31:10 +0530

    Added login function

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ function signup(userName){
 
 console.log(signup("john"))
 console.log(signup("ravi"))
+
+
+function login(userName,password){
+    let registeredUsers=["swetha","john","swathi"]
+    if(!registeredUsers.includes(userName)){
+        return "User Not Found, Please signup"
+    }else if(password!="Emp@123"){
+        return "Wrong Password...."
+    }else{
+        return "Login Successfull...."
+    }
+}
+
+console.log(login("swetha","Emp@123"))
+console.log(login("john","1234"))
+console.log(login("swathi","Emp@123"))
